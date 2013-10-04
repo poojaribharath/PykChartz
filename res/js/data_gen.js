10ddd73389d746cc48cfb885dff8d35564b59e7e
Prateek Saxena <prtksxna@gmail.com>
2013-10-04 10:02:58 +0530

    Adding the sample data and cleaning up links

--- res/js/data_gen.js
+++ res/js/data_gen.js
@@ -1,123 +1,155 @@
-function getData(){
-    var state_names = [
-	"AL",
-	"AK",
-	"AZ",
-	"AR",
-	"CA",
-	"CO",
-	"CT",
-	"DE",
-	"DC",
-	"FL",
-	"GA",
-	"HI",
-	"ID",
-	"IL",
-	"IN",
-	"IA",
-	"KS",
-	"KY",
-	"LA",
-	"ME",
-	"MD",
-	"MA",
-	"MI",
-	"MN",
-	"MS",
-	"MO",
-	"MT",
-	"NE",
-	"NV",
-	"NH",
-	"NJ",
-	"NM",
-	"NY",
-	"NC",
-	"ND",
-	"OH",
-	"OK",
-	"OR",
-	"PA",
-	"RI",
-	"SC",
-	"SD",
-	"TN",
-	"TX",
-	"UT",
-	"VT",
-	"VA",
-	"WA",
-	"WV",
-	"WI",
-	"WY",
-	"PR",
-	"AL",
-	"AK",
-	"AZ",
-	"AR",
-	"CA",
-	"CO",
-	"CT",
-	"DE",
-	"DC",
-	"FL",
-	"GA",
-	"HI",
-	"ID",
-	"IL",
-	"IN",
-	"IA",
-	"KS",
-	"KY",
-	"LA",
-	"ME",
-	"MD",
-	"MA",
-	"MI",
-	"MN",
-	"MS",
-	"MO",
-	"MT",
-	"NE",
-	"NV",
-	"NH",
-	"NJ",
-	"NM",
-	"NY",
-	"NC",
-	"ND",
-	"OH",
-	"OK",
-	"OR",
-	"PA",
-	"RI",
-	"SC",
-	"SD",
-	"TN",
-	"TX",
-	"UT",
-	"VT",
-	"VA",
-	"WA",
-	"WV",
-	"WI",
-	"WY",
-	"PR",
-	"AL"
-    ]
-    var data = [];
-    
-    for(var i = 1950; i <= 2013; i++){
-        for(var j = 0; j < state_names.length; j++){
-            var obj = {
-                "state": state_names[j],
-                "population": parseInt(Math.random() * (j + 1) * i),
-                "year": i
-            };
-            data.push(obj);
-        };
-    };
-    
-    return data;
-}
+sample_data =  [
+    {
+	id: 1,
+	display_name: "Outreach",
+	technical_name: "outreach",
+	duration: "3 mins",
+	breakup: [
+            {
+		name: "Scans",
+		color: "gray",
+		count: 500,
+		tooltip: "hihi-scans"
+            },
+            {
+		name: "Facebook",
+		color: "#1D3174",
+		count: 100,
+		tooltip: "hihi-fb"
+            },
+            {
+		name: "Twitter",
+		color: "#3998DC",
+		count: 40,
+		tooltip: "hihi-tw"
+            },
+            {
+		name: "SalesTag",
+		color: "#E04416",
+		count: 20,
+		tooltip: "hihi-st"
+            },
+            {
+		name: "GooglePlus",
+		color: "#BD341C",
+		count: 10,
+		tooltip: "hihi-gp"
+            },
+            {
+		name: "Pinterest",
+		color: "#B20D1B",
+		count: 30,
+		tooltip: "hihi-pi"
+            },
+            {
+		name: "Email",
+		color: "#C1BF00",
+		count: 29,
+		tooltip: "hihi-em"
+            },
+
+	]
+    },
+    {
+	id: 2,
+	display_name: "Interest",
+	technical_name: "Browses",
+	duration: "2 mins",
+	breakup: [
+            {
+		name: "Scans",
+		color: "gray",
+		count: 100,
+		tooltip: "hihi-scans"
+            },
+            {
+		name: "Facebook",
+		color: "#1D3174",
+		count: 20,
+		tooltip: "hihi-fb"
+            },
+            {
+		name: "Twitter",
+		color: "#3998DC",
+		count: 8,
+		tooltip: "hihi-tw"
+            },
+            {
+		name: "SalesTag",
+		color: "#E04416",
+		count: 4,
+		tooltip: "hihi-st"
+            },
+            {
+		name: "GooglePlus",
+		color: "#BD341C",
+		count: 3,
+		tooltip: "hihi-gp"
+            },
+            {
+		name: "Pinterest",
+		color: "#B20D1B",
+		count: 6,
+		tooltip: "hihi-pi"
+            },
+            {
+		name: "Email",
+		color: "#C1BF00",
+		count: 6,
+		tooltip: "hihi-em"
+            },
+
+	]
+    },
+    {
+	id: 3,
+	display_name: "Desire",
+	technical_name: "Add to Cart",
+	duration: "2 mins",
+	breakup: [
+            {
+		name: "Scans",
+		color: "gray",
+		count: 50,
+		tooltip: "hihi-scans"
+            },
+            {
+		name: "Facebook",
+		color: "#1D3174",
+		count: 10,
+		tooltip: "hihi-fb"
+            },
+            {
+		name: "Twitter",
+		color: "#3998DC",
+		count: 5,
+		tooltip: "hihi-tw"
+            },
+            {
+		name: "SalesTag",
+		color: "#E04416",
+		count: 3,
+		tooltip: "hihi-st"
+            },
+            {
+		name: "GooglePlus",
+		color: "#BD341C",
+		count: 2,
+		tooltip: "hihi-gp"
+            },
+            {
+		name: "Pinterest",
+		color: "#B20D1B",
+		count: 4,
+		tooltip: "hihi-pi"
+            },
+            {
+		name: "Email",
+		color: "#C1BF00",
+		count: 6,
+		tooltip: "hihi-em"
+            },
+
+	]
+    }
+]
