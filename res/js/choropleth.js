40203dd88eec3bec89bcea5071e26dc4e79e0db2
Prateek Saxena <prtksxna@gmail.com>
2013-10-10 13:18:12 +0530

    Switching between layers instead of loading new paths. Much faster

--- res/js/choropleth.js
+++ res/js/choropleth.js
@@ -6,7 +6,7 @@ var state_data = []
 for(var i = 0; i < 50; i++) state_data.push(Math.random());
 
 var county_data = []
-for(var i = 0; i < 5000; i++) county_data.push(Math.random());
+for(var i = 0; i < 10000; i++) county_data.push(Math.random());
 
 
 $(document).ready(function(){
@@ -16,21 +16,35 @@ $(document).ready(function(){
 	.attr("width", width)
 	.attr("height", height);
 
-    var g = svg.append("g");
+    var counties_g = svg.append("g").attr("class","counties");
+    var states_g = svg.append("g").attr("class","states");
 
     d3.json("/res/data/us.json", function(error, topology) {
-	g.selectAll("path")
+
+	counties_g.selectAll("path")
+	    .data(topojson.feature(topology, topology.objects.counties).features)
+	    .enter().append("path").attr("class", "county")
+	    .attr("d", path)
+	    .attr("style", function(d, i){
+		return "opacity: " + county_data[i];
+	    })
+	    .on("click", unzoom);
+
+	states_g.selectAll("path")
 	    .data(topojson.feature(topology, topology.objects.states).features)
 	    .enter().append("path").attr("class", "state")
 	    .attr("d", path)
 	    .attr("style", function(d, i){
 		return "opacity: " + state_data[i];
 	    })
+	    .attr("data-heat", function(d, i){
+		return state_data[i];
+	    })
 	    .on("click", zoom);
 
+	$("g.counties").hide();
 
 	function zoom(d){
-	    console.log(d);
 	    var x, y, k;
 
 	    var centroid = path.centroid(d);
@@ -39,31 +53,21 @@ $(document).ready(function(){
 	    k = 4;
 	    centered = d;
 
+	    $("g.states path").css("fill", "none");
+	    $("g.states path").animate({opacity: 1});
+	    $("g.states path").css("stroke-width", "3px");
+	    $("g.states path").css("stroke", "#fff");
+	    $("g.counties").fadeIn();
 
-	    g.selectAll("path").remove();
 
-	    g.selectAll("path")
-		.data(topojson.feature(topology, topology.objects.counties).features)
-		.enter().append("path").attr("class", "county")
-		.attr("d", path)
-		.attr("style", function(d, i){
-		    return "opacity: " + county_data[i];
-		})
-		.on("click", unzoom);
-
-	    g.selectAll("path")
-		.data(topojson.feature(topology, topology.objects.states).features)
-		.enter().append("path").attr("class", "state")
-		.attr("d", path)
-		.attr("style", function(d, i){
-		    return "fill: transparent";
-		})
-		.on("click", zoom);
-
-	    g.transition()
+	    counties_g.transition()
 		.duration(750)
-		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-		.style("stroke-width", 1.5 / k + "px");
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
+
+
+	    states_g.transition()
+		.duration(750)
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
 	}
 
 	function unzoom(d){
@@ -71,22 +75,21 @@ $(document).ready(function(){
 	    x = width/2;
 	    y = height/2;
 	    k = 1;
+	    $("g.counties").fadeOut();
+	    $("g.states path").css("fill", "#000");
+	    $("g.states path").css("stroke-width", "0");
+	    $("g.states path").css("opacity", function(){
+		return $(this).attr("data-heat");
+	    });
 
-	    g.selectAll("path").remove();
-
-	    g.selectAll("path")
-		.data(topojson.feature(topology, topology.objects.states).features)
-		.enter().append("path").attr("class", "state")
-		.attr("d", path)
-		.attr("style", function(d, i){
-		    return "opacity: " + state_data[i];
-		})
-		.on("click", zoom);
-
-	    g.transition()
+	    states_g.transition()
 		.duration(750)
-		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-		.style("stroke-width", 1.5 / k + "px");
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
+
+	    counties_g.transition()
+		.duration(750)
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
+
 
 	}
 
