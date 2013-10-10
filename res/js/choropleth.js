0ca2915ac3d894b7c704a341626a2694a27e7528
Prateek Saxena <prtksxna@gmail.com>
2013-10-10 11:11:17 +0530

    Adding stuff for the choropleth chart

---
+++ res/js/choropleth.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var width = 960,
+    height = 500;
+
+    var path = d3.geo.path();
+
+    var svg = d3.select("#choropleth-container").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+    d3.json("/res/data/us.json", function(error, topology) {
+	svg.selectAll("path")
+	    .data(topojson.feature(topology, topology.objects.counties).features)
+	    .enter().append("path")
+	    .attr("d", path);
+    });
+});
