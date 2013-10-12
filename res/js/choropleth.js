320694f147c208738191dd6a38f4cf74189eaccc
Prateek Saxena <prtksxna@gmail.com>
2013-10-12 19:47:44 +0530

    Showing the neighnouring states a bit more. #20

--- res/js/choropleth.js
+++ res/js/choropleth.js
@@ -181,7 +181,7 @@ Choropleth = function(options){
 
 		$("g.states path").css("fill", "#fff"); // Conceal all states
 		$(this).css("fill", "none"); // Show the one that has been clicked on
-		$("g.states path").animate({opacity: 1});
+		$("g.states path").animate({opacity: 0.7});
 		$("g.counties").show();
 	    });
 
