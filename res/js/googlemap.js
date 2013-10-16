45ab2955e8a0b40e86231eb5f2f9f234b5342ae2
Prateek Saxena <prtksxna@gmail.com>
2013-10-16 16:50:03 +0530

    Setting up a basic heatmap

--- res/js/googlemap.js
+++ res/js/googlemap.js
@@ -7,20 +7,51 @@ Pyk.GoogleHeat = function(options){
 	    .css("width", this.options.width + "px");
 
 	this.container.append(this.div);
-	this.render();
+
+	var that = this;
+
+	$.getJSON(this.options.data, function(data){
+	    that.data = data;
+	    that.render();
+	});
     }
 
     this.render = function(){
 	var div = this.div.get(0);
 
 	var mapOptions = {
-            center: new google.maps.LatLng(-34.397, 150.644),
-            zoom: 8,
+            center: new google.maps.LatLng(45,90),
+            zoom: 1,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(div, mapOptions);
+
+        this.map = new google.maps.Map(div, mapOptions);
+
+	this.setupHeat();
     }
 
+    this.setupHeat = function(){
+	var pointArray = new google.maps.MVCArray(this.heatData());
+	var heatmap = new google.maps.visualization.HeatmapLayer({
+	    data: pointArray
+	});
+	heatmap.setMap(this.map);
+
+    }
+
+    this.heatData = function(){
+	var d = []
+
+	for(i in this.data){
+	    var p = this.data[i];
+	    var o = new google.maps.LatLng(p.latitude, p.longitude);
+	    d.push({location: o, weight: p.count});
+	}
+	console.log(d);
+	return d;
+    }
+
+    // Defatuls and Validations for the Options
     this.validate_options = function(){
 	if(this.options.selection == undefined) return false;
 	if(this.options.data == undefined) return false;
