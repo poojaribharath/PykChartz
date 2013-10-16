e38bb246061f18eba537a663e0fb3d8b41cac759
Prateek Saxena <prtksxna@gmail.com>
2013-10-16 16:32:12 +0530

    Setting up basic objects for #24

--- res/js/googlemap.js
+++ res/js/googlemap.js
@@ -1,10 +1,36 @@
-function initialize() {
-    var mapOptions = {
-	center: new google.maps.LatLng(-34.397, 150.644),
-	zoom: 8,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-				  mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+Pyk.GoogleHeat = function(options){
+    this.init = function(){
+	if(!this.validate_options()) return false;
+	this.container = $(this.options.selection);
+	this.div = $("<div>")
+	    .css("height", this.options.height + "px")
+	    .css("width", this.options.width + "px");
+
+	this.container.append(this.div);
+	this.render();
+    }
+
+    this.render = function(){
+	var div = this.div.get(0);
+
+	var mapOptions = {
+            center: new google.maps.LatLng(-34.397, 150.644),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(div, mapOptions);
+    }
+
+    this.validate_options = function(){
+	if(this.options.selection == undefined) return false;
+	if(this.options.data == undefined) return false;
+	return true;
+    }
+
+    this.options = jQuery.extend({
+	width: 960,
+	height: 500
+    }, options);
+
+    return this;
+};
