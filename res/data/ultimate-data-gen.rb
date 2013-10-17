32d6c75cf835a2b9e5e4434454116874a106e6b2
Prateek Saxena <prtksxna@gmail.com>
2013-10-17 14:31:10 +0530

    Creating a generator for the ultimate bar chart

---
+++ res/data/ultimate-data-gen.rb
@@ -0,0 +1,44 @@
+top_cat = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+under_cat = ["Previous", "Current", "Next", "Predicted"]
+data_cat = ["Missed", "Achieved", "Over Achieved", "Potato"]
+colors = ["#1E76B4", "#FF7F0D", "#2CA02C", "#D62728"]
+tooltips = [
+            "The light of a hundred stars does not equal the light of the moon.",
+            "Don't look now, but there is a multi-legged creature on your shoulder.",
+            "today: A nice place to visit, but you can't stay here for long.",
+            "Have at you!",
+            "Needs are a function of what other people have."
+           ]
+
+data = []
+
+top_cat.each do |tc|
+  t_data = []
+
+  under_cat.each do |uc|
+    if((rand * under_cat.length) > 1)
+      uc_data = []
+
+      data_cat.each_with_index do |dc,i|
+        if((rand * data_cat.length) > 1)
+
+          dc_data = {
+            "name" => dc,
+            "val" => (rand * 100).floor,
+            "color" => colors[i],
+            "tooltip" => tooltips[(rand * 5).floor]
+          }
+          uc_data.push({dc => dc_data})
+
+        end
+      end
+
+      t_data.push({uc => uc_data})
+    end
+  end
+
+  data.push({tc => t_data})
+end
+
+
+puts data.to_s.gsub("=>", ":")
