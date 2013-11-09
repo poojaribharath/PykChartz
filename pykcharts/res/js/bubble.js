bc32b99e345669cdd958eb2e2f3fb5641cc26e7e
gehlotchirag <gehlotchirag1988@gmail.com>
2013-11-09 19:33:50 +0530

    js code rectified

---
+++ pykcharts/res/js/bubble.js
@@ -0,0 +1,150 @@
+PykCharts.BubblePack = function (options) {
+
+    //----------------------------------------------------------------------------------------
+    //1. This is the method that executes the various JS functions in the proper sequence to generate the chart
+    //----------------------------------------------------------------------------------------
+    this.execute = function () {
+        //1.3 Assign Global variable var that to access function and variable throughout   
+        var that = this;
+
+        // 1.2 Read Json File Get all the data and pass to render
+        d3.json(options.data, function (e, data) {
+            that.data = data;
+            that.render();
+            that.renderTooltip();
+
+        });
+    }
+
+
+    //----------------------------------------------------------------------------------------
+    //2. Render function to create the chart
+    //----------------------------------------------------------------------------------------
+    this.render = function () {
+        var that = this;
+
+        // 2.1 Set diameter and color scale
+        var diameter = 600,
+            format = d3.format(",d"),
+            color = d3.scale.category20c();
+
+
+        // 2.2 Set d3 Layout as bubble
+        var bubble = d3.layout.pack()
+            .sort(null)
+            .size([diameter, diameter])
+            .padding(1.5);
+
+        // 2.3 Append Svg holder 
+        var svg = d3.select(options.selection).append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .attr("class", "bubble");
+
+        var CircleScale = d3.scale.linear()
+            .domain([0, 10000])
+            .range([0, diameter]);
+
+
+        d3.json(options.data, function (error, root) {
+            var node = svg.selectAll(".node")
+                .data(bubble.nodes(classes(root))
+                    .filter(function (d) {
+                        return !d.children;
+                    }))
+                .enter().append("g")
+                .attr("class", "node")
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
+
+
+            node.append("title")
+                .text(function (d) {
+                    return d.className + ": " + format(d.value);
+                });
+
+            node.append("circle")
+                .attr("r", function (d) {
+                    return d.r;
+                })
+                .on("mouseover", function (d) {
+                    return that.tooltip.html(d.tip).style("visibility", "visible");
+                })
+            //	.on("mouseover", function(d){return   console.log("tooltip:" + d.tip);})
+
+            .on("mousemove", function () {
+                return that.tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
+            })
+
+            .on("mouseout", function () {
+                return that.tooltip.style("visibility", "hidden");
+            })
+
+            .style("fill", function (d) {
+                return color(d.packageName);
+            });
+
+            node.append("text")
+                .attr("dy", ".3em")
+                .style("text-anchor", "middle")
+                .text(function (d) {
+                    return d.className.substring(0, d.r / 3);
+                });
+        });
+
+        // Returns a flattened hierarchy containing all leaf nodes under the root.
+        function classes(root) {
+            var classes = [];
+
+            function recurse(name, node) {
+                if (node.children) node.children.forEach(function (child) {
+                    recurse(node.name, child);
+                });
+                else {
+                    console.log("className:" + node);
+                    console.log("ttip:" + node.ttip);
+                    console.log("color:" + node.colors);
+
+                    classes.push({
+                        packageName: name,
+                        className: node.name,
+                        value: node.size,
+                        tip: node.ttip,
+                        color: node.colors
+                    });
+                }
+            }
+
+            recurse(null, root);
+            console.log("classes:" + classes.ttip);
+            return {
+                children: classes
+            };
+        }
+
+        d3.select(self.frameElement).style("height", diameter + "px");
+
+    }
+
+
+    //----------------------------------------------------------------------------------------
+    //3. Render function to create the chart
+    //----------------------------------------------------------------------------------------
+
+    this.renderTooltip = function () {
+
+        $("#pyk-bubble-tooltip").remove();
+        this.tooltip = d3.select("body")
+            .append("div").attr("id", "pyk-bubble-tooltip")
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("visibility", "hidden")
+            .style("background", "#fff")
+            .style("padding", "10px 20px")
+            .style("box-shadow", "0 0 10px #000")
+            .style("border-radius", "5px")
+            .text("a simple tooltip");
+
+    }
+}
---
+++ pykcharts/res/js/treerect.js
@@ -0,0 +1,159 @@
+PykCharts.treerect = function (options) {
+
+
+    //----------------------------------------------------------------------------------------
+    //1. This is the method that executes the various JS functions in the proper sequence to generate the chart
+    //----------------------------------------------------------------------------------------
+    this.execute = function () {
+        //1.1 Assign Global variable var that to access function and variable throughout   
+        var that = this;
+
+        //1.2 set width height to local variable   
+        width = this.options.width,
+        height = this.options.height;
+
+        // 1.3 Read Json File Get all the data and pass to render
+        d3.json(options.data, function (e, data) {
+            that.data = data;
+            that.render();
+            that.renderTooltip();
+
+        });
+    }
+
+    //----------------------------------------------------------------------------------------
+    //2. Render chart
+    //----------------------------------------------------------------------------------------
+    this.render = function () {
+        var that = this;
+
+        //2.1 Assign height and width to a local variable because if you are manipulating with h and w then the SVG height and width will not get affected
+        var w = width - 60,
+            h = height - 135,
+            x = d3.scale.linear().range([0, w]),
+            y = d3.scale.linear().range([0, h]),
+            root,
+            node;
+
+        //2.2 Set D3 layout as tree layout
+        var treemap = d3.layout.treemap()
+            .round(false)
+            .size([w, h])
+            .sticky(true)
+            .value(function (d) {
+                return d.size;
+            });
+
+        //2.3 SVG Holder for Chart
+        var svg = d3.select(options.selection).append("div")
+            .attr("class", "chart")
+            .style("width", w + "px")
+            .style("height", h + "px")
+            .append("svg:svg")
+            .attr("width", w)
+            .attr("height", h)
+            .append("svg:g")
+            .attr("transform", "translate(.5,.5)");
+
+        d3.json(options.data, function (data) {
+            node = root = data;
+
+            var nodes = treemap.nodes(root)
+                .filter(function (d) {
+                    return !d.children;
+                });
+
+            //2.4 Append group element to hold chart
+            var cell = svg.selectAll("g")
+                .data(nodes)
+                .enter().append("svg:g")
+                .attr("class", "cell")
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
+
+            //2.5 Append rect element to chart
+            cell.append("svg:rect")
+                .attr("width", function (d) {
+                    return d.dx - 1;
+                })
+                .attr("height", function (d) {
+                    return d.dy - 1;
+                })
+                .on("mouseover", function (d) {
+                    return that.tooltip.html(d.ttip).style("visibility", "visible");
+                })
+                .on("mousemove", function () {
+                    return that.tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
+                })
+                .on("mouseout", function () {
+                    return that.tooltip.style("visibility", "hidden");
+                })
+                .style("fill", function (d) {
+                    return d.colors;
+                });
+
+
+            //2.6 Append logos element to rect
+            cell.append("image")
+                .attr("xlink:href", function (d) {
+                    return d.img;
+                })
+                .attr("x", 10)
+                .attr("y", 7)
+                .attr("width", 17.92)
+                .attr("height", 16);
+
+
+
+            //2.6 Append text element to chart
+            cell.append("svg:text")
+                .attr("x", function (d) {
+                    return d.dx / 2;
+                })
+                .attr("y", function (d) {
+                    return d.dy / 2;
+                })
+                .attr("dy", ".35em")
+                .attr("text-anchor", "middle")
+                .text(function (d) {
+                    return d.name;
+                })
+                .attr("fill", "white")
+                .style("opacity", function (d) {
+                    d.w = this.getComputedTextLength();
+                    return d.dx > d.w ? 1 : 0;
+                });
+
+        });
+
+        function size(d) {
+            return d.size;
+        }
+
+        function count(d) {
+            return 1;
+        }
+
+    }
+
+    //----------------------------------------------------------------------------------------
+    //3. Render Tooltip
+    //----------------------------------------------------------------------------------------
+    this.renderTooltip = function () {
+
+        $("#pyk-tree-tooltip").remove();
+        this.tooltip = d3.select("body")
+            .append("div").attr("id", "pyk-tree-tooltip")
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("visibility", "hidden")
+            .style("background", "#fff")
+            .style("padding", "10px 20px")
+            .style("box-shadow", "0 0 10px #000")
+            .style("border-radius", "5px")
+            .text("a simple tooltip");
+
+    }
+
+}
